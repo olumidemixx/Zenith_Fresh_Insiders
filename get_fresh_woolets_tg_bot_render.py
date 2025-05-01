7b827459fd1f64db1cb59d52383058b2af2f540b
olumidemixx <OLUMIDEMIXX@GMAIL.COM>
2025-05-01 10:49:17 +0100

    Update get_fresh_woolets_tg_bot_render.py

--- get_fresh_woolets_tg_bot_render.py
+++ get_fresh_woolets_tg_bot_render.py
@@ -91,7 +91,7 @@ async def on_startup(web_app):
         await application.start()
         
         # Use Render URL from environment variable
-        webhook_url = "https://zenith-fresh-insiders.onrender.com"
+        webhook_url = "https://zenith-fresh-insiders-3lpx.onrender.com"
         if not webhook_url:
             logging.error("RENDER_EXTERNAL_URL environment variable not found")
             await application.shutdown()
